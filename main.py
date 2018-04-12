e3b8b0615a0dac0ca4becb2a19809a7da00ffd6f
raisonm <mattandthings@gmail.com>
2018-04-12 00:09:43 -0400

    converting this into a calculator

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
 import kivy
 kivy.require('1.0.9')
-from kivy.lang import Builder
-from kivy.uix.gridlayout import GridLayout
-from kivy.properties import NumericProperty
 from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
 
-Builder.load_string('''
-<HelloWorldScreen>:
-    cols: 1
-    Label:
-        text: 'Welcome to the Hello world'
-    Button:
-        text: 'Click me! %d' % root.counter
-        on_release: root.my_callback()
-''')
 
-class HelloWorldScreen(GridLayout):
-    counter = NumericProperty(0)
-    def my_callback(self):
-        print 'The button has been pushed'
-        self.counter += 1
+class CalcGridLayout(GridLayout):
+    def calculate(self, calculation):
+        if calculation:
+            try:
+                self.display.text = str(eval(calculation))
+            except Exception:
+                self.display.text = "Error"
 
-class HelloWorldApp(App):
+
+class CalculatorApp(App):
+
     def build(self):
-        return HelloWorldScreen()
+        return CalcGridLayout()
 
-if __name__ == '__main__':
-    HelloWorldApp().run()
+
+calcApp = CalculatorApp()
+calcApp.run()
